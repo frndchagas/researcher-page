d3c28b0f030591dfbf46a30805a5c16754aaef3a
Fernando Chagas <fnd.chagas@gmail.com>
2022-02-07 20:51:57 -0300

    Researcher page Example

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#8c0333",
+        secondary: "#732540",
+      },
+    },
+    fontFamily: {
+      sans: ["Titillium Web", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
